794a1ea210820c916d9efbf41d01851296ba73f2
MshSiam <siyamkenway@gmail.com>
2022-07-01 11:19:10 +0600

    http request with route parameters

--- Express js/http request/index.js
+++ Express js/http request/index.js
@@ -6,14 +6,27 @@ const server = app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`)
 })
 
-// send value using query parameters.
+// ---------------------send value using query parameters. --------------------
 
+// app.get("/", (req, res) => {
+//   const id = req.query.id
+//   const age = req.query.age
+//   res.send(`<h1>user id is : ${id}</h1>
+//   <h2>age : ${age}</h2>
+//   `)
+// })
+
+// ------------------route parameter --------------------------
+
+// app.get("/users/:id/age/:age", (req, res) => {
+//   const id = req.params.id
+//   const age = req.params.age
+//   res.send(`<h3>User id is : ${id} and age is :${age}</h3>`)
+// })
+
+//  -------------- send valuse using headers ---------------
 app.get("/", (req, res) => {
-  const id = req.query.id
-  const age = req.query.age
-  res.send(`<h1>user id is : ${id}</h1>
-  <h2>age : ${age}</h2>
-  `)
+  const id = req.header("id")
+  const name = req.header("name")
+  res.send(`<h3>User id is : ${id} and name is :${name}</h3>`)
 })
-
-// route parameter
