624cf774f2bd5ac65ca9f7da161ec51b42c6c383
MshSiam <siyamkenway@gmail.com>
2022-07-01 15:51:39 +0600

    send and recive data in form

--- Express js/http request/index.js
+++ Express js/http request/index.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.local || 3000
+const bodyParser = require("body-parser")
 
-const server = app.listen(PORT, () => {
-  console.log(`server is running at http://localhost:${PORT}`)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.json())
+
+// =============== Make post request with json or format data ===========================
+// error fixed.
+
+app.post("/user", (req, res) => {
+  const name = req.body.name
+  const age = req.body.age
+  res.send(`welcome ${name}. he is ${age} years old`)
 })
 
 // ---------------------send value using query parameters. --------------------
@@ -16,6 +26,8 @@ const server = app.listen(PORT, () => {
 //   `)
 // })
 
+// =================================================================
+
 // ------------------route parameter --------------------------
 
 // app.get("/users/:id/age/:age", (req, res) => {
@@ -25,8 +37,27 @@ const server = app.listen(PORT, () => {
 // })
 
 //  -------------- send valuse using headers ---------------
-app.get("/", (req, res) => {
-  const id = req.header("id")
-  const name = req.header("name")
-  res.send(`<h3>User id is : ${id} and name is :${name}</h3>`)
+// app.get("/", (req, res) => {
+//   const id = req.header("id")
+//   const name = req.header("name")
+//   res.send(`<h3>User id is : ${id} and name is :${name}</h3>`)
+// })
+
+// =============================================================
+// send and recive data in form-------------------------
+
+// -----------get------------
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/form.html")
+})
+
+// ----------post-------------
+app.post("/register", (req, res) => {
+  const name = req.body.fullName
+  const age = req.body.age
+  res.send(`<h2>Your name is ${name}, age ${age}</h2>`)
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`)
 })
