2aa0524e82e5e82a6827a111c0aff0b711dbd82c
MshSiam <siyamkenway@gmail.com>
2022-07-17 15:21:55 +0600

    rout set up done

--- Passport local strategy session base/app.js
+++ Passport local strategy session base/app.js
@@ -1,5 +1,48 @@
-require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
+const ejs = require("ejs")
 const app = express()
+
+app.set("view engine", "ejs")
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// --------- base url -------------//
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+// --------- register : get ------------//
+app.get("/register", (req, res) => {
+  res.render("register")
+})
+
+// --------- register : post ------------//
+app.post("/register", (req, res) => {
+  try {
+    res.status(201).send("user is created")
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
+// --------- login : get ------------//
+app.get("/login", (req, res) => {
+  res.render("login")
+})
+// --------- login : post ------------//
+app.post("/login", (req, res) => {
+  try {
+    res.status(200).send("user is loggedin")
+  } catch (error) {
+    res.status(500).send(error.message)
+  }
+})
+
+// --------- profile : protected route ------------//
+app.get("/profile", (req, res) => {
+  res.render("profile")
+})
 
 module.exports = app
